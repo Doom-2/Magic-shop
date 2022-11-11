061243161c32d2405e2f058f58989aaff1751d75
doom <phramov945@gmail.com>
2022-11-11 20:05:32 +0300

    models User, Location were added

--- ads/models.py
+++ ads/models.py
@@ -1,14 +1,60 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+    class Meta:
+        verbose_name = "Локация"
+        verbose_name_plural = "Локации"
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    role = models.CharField(max_length=15)
+    age = models.PositiveSmallIntegerField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
+
+
 class Ad(models.Model):
     name = models.CharField(max_length=100)
-    author = models.CharField(max_length=50, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.PositiveIntegerField(null=True, blank=True)
     description = models.CharField(max_length=2000)
     address = models.CharField(max_length=200)
     is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='logos/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
