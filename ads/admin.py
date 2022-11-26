7ac03d8a4ed0e35d1d69dea2c97524d224e541ff
doom <phramov945@gmail.com>
2022-11-26 14:16:57 +0300

    Selection model was registered

--- ads/admin.py
+++ ads/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from ads.models import Category, Location, User, Ad
+from ads.models import Category, Location, User, Ad, Selection
 
 admin.site.register(Category)
 admin.site.register(Location)
 admin.site.register(User)
 admin.site.register(Ad)
+admin.site.register(Selection)
